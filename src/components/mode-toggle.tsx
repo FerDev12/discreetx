5c0d437cfb71181541de927474d643a34efa5a18
FerDev12 <fernando.mu.dev@gmail.com>
2023-08-29 16:49:14 -0600

    added user button and mode toggle to sidebar navigation

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -18,7 +18,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
+        <Button
+          variant='outline'
+          // size='icon'
+          className='w-[48px] h-[48px]'
+        >
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
@@ -27,21 +31,21 @@ export function ModeToggle() {
       <DropdownMenuContent align='end'>
         <DropdownMenuItem
           onClick={() => setTheme('light')}
-          className='flex justify-between'
+          className='flex justify-between cursor-pointer'
         >
           Light
           {theme === 'light' && <Check className='w-4 h-4' />}
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('dark')}
-          className='flex justify-between'
+          className='flex justify-between cursor-pointer'
         >
           Dark
           {theme === 'dark' && <Check className='w-4 h-4' />}
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('system')}
-          className='flex justify-between'
+          className='flex justify-between cursor-pointer'
         >
           System
           {theme === 'system' && <Check className='w-4 h-4' />}
