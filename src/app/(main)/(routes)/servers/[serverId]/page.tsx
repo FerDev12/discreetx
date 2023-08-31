0620f8ff3f993bc444fabab5693806559e654963
FerDev12 <fernando.mu.dev@gmail.com>
2023-08-31 10:21:46 -0600

    channel and conversation pages

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,31 @@
+import { redirect } from 'next/navigation';
+import { redirectToSignIn } from '@clerk/nextjs';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+
 type ServerIdPageProps = {
   params: { serverId: string };
 };
 
 export default async function ServerIdPage({ params }: ServerIdPageProps) {
-  return <div>SeverId Page</div>;
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findFirst({
+    where: {
+      profileId: profile.id,
+      name: 'general',
+      serverId: params.serverId,
+    },
+  });
+
+  if (channel) {
+    return redirect(`/servers/${params.serverId}/channels/${channel.id}`);
+  }
+
+  return null;
 }
