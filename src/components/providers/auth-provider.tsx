a41d2a4e2513f55e4c22a7b57e419a9c33fce4df
FerDev12 <fernando.mu.dev@gmail.com>
2023-09-18 18:59:51 -0600

    feat: removed dark theme from clerk provider

--- src/components/providers/auth-provider.tsx
+++ src/components/providers/auth-provider.tsx
@@ -1,13 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import { ReactNode } from 'react';
-import { dark } from '@clerk/themes';
+// import { dark } from '@clerk/themes';
 
 export async function AuthProvider({ children }: { children: ReactNode }) {
   return (
     <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
+    // appearance={{
+    //   baseTheme: dark,
+    // }}
     >
       {children}
     </ClerkProvider>
